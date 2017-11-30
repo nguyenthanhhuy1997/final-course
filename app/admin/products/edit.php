2d57df3e546059c50857d353c1f87a32d82f4f58
Thanh Huy <nguyenthanhhuy1997@gmail.com>
2017-11-30 11:06:56 +0700

    finish admin-products

---
+++ app/admin/products/edit.php
@@ -0,0 +1,79 @@
+<?php session_start(); ?>
+<?php require_once "../../../db/mysql.php"; ?>
+<?php
+  if(!isset($_GET["id"])){
+    $_SESSION["flash"] = "Wrong params";
+    header("location: index.php");
+  }
+
+  $id = $_GET["id"];
+  $sql = "select * from products where id=$id";
+  $result = $conn->query($sql);
+  $row = $result->fetch_assoc();
+  $catalog_id = $row["catalog_id"];
+  $name = $row["name"];
+  $description = $row["description"];
+  $image = $row["image"];
+  $qty = $row["qty"];
+  $price = $row["price"];
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Chỉnh sửa sản phẩm</title>
+    <link rel="stylesheet" type="text/css" href="../../../public/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../public/css/custom.css">
+  </head>
+  <body>
+  <div class="wrapper">
+    <form method="post" action="update.php">
+      <input type="hidden" value="<?php echo $id; ?>" name="id">
+      <div class="row">
+        <i class="flash"><?php if(isset($_SESSION["flash"])) echo $_SESSION["flash"]; ?></i>
+      </div>
+      <div>
+        <h1>Chỉnh sửa</h1>
+      </div>
+      <div class="row">
+        <label>Danh mục:</label>
+        <select class="form-control" name="catalog_id">
+          <?php
+            $sql = "select * from catalogs";
+            $result = $conn->query($sql);
+            if($result->num_rows > 0){
+              while($row = $result->fetch_assoc()){ ?>
+                  <option <?php if($catalog_id == $row['id']) echo 'selected=true'; ?> value="<?php echo $row['id']; ?>"><?php echo $row["name"]; ?></option>
+          <?php    }
+            }
+          ?>
+        </select>
+      </div>
+      <div class="row">
+        <label>Tên sản phẩm:</label>
+        <input class="form-control" type="text" name="name" value="<?php echo $name; ?>">
+      </div>
+      <div class="row">
+        <label>Mô tả:</label>
+        <textarea class="form-control" name="description"><?php echo $description; ?></textarea>
+      </div>
+      <div class="row">
+        <label>Ảnh:</label>
+        <input class="form-control" type="text" name="image" value="<?php echo $image; ?>">
+      </div>
+      <div class="row">
+        <label>Số lượng:</label>
+        <input class="form-control" type="text" name="qty" value="<?php echo $qty; ?>">
+      </div>
+      <div class="row">
+        <label>Giá sản phẩm:</label>
+        <input class="form-control" type="text" name="price" value="<?php echo $price; ?>">
+      </div>
+      <br>
+      <div class="row">
+        <button class="btn btn-primary">Submit</button>
+      </div>
+    </form>
+  </div>
+  </body>
+</html>
+<?php unset($_SESSION["flash"]); ?>
---
+++ app/admin/products/index.php
@@ -0,0 +1,56 @@
+<?php session_start(); ?>
+<?php require_once "../../../db/mysql.php"; ?>
+<?php
+  define("URL_IMAGE", "http://localhost:8080/final-course/public/uploads/");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Danh sách sản phẩm</title>
+  <link rel="stylesheet" type="text/css" href="../../../public/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../../../public/css/custom.css">
+</head>
+<body>
+  <div class="container">
+  <h2>Danh sách</h2>
+  <div class="row">
+    <i class="flash"><?php if(isset($_SESSION["flash"])) echo $_SESSION["flash"]; ?></i>
+  </div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Ảnh</th>
+        <th>Tên sản phẩm</th>
+        <th>Mô tả</th>
+        <th>Số lượng  </th>
+        <th>Giá</th>
+        <th>Chỉnh sửa</th>
+        <th>Xóa</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php
+      $sql = "select * from products";
+      $result = $conn->query($sql);
+      if($result->num_rows > 0)
+      {
+        while($row = $result->fetch_assoc()) {?>
+        <tr>
+          <td><img src="<?php echo URL_IMAGE.$row['image'];?>" width="32" height="32"></td>
+          <td><?php echo $row["name"];?></td>
+          <td><?php echo $row["description"];?></td>
+          <td><?php echo $row["qty"];?></td>
+          <td><?php echo $row["price"];?></td>
+          <td><a href="edit.php?id=<?php echo $row["id"];?>">Edit</a></td>
+          <td><a href="delete.php?id=<?php echo $row["id"];?>">Delete</a></td>
+          </tr>
+        <?php  }
+      }
+
+    ?>
+    </tbody>
+  </table>
+</div>
+</body>
+</html>
