a199392a0d97f72527185a39799bdba525f834af
Thanh Huy <nguyenthanhhuy1997@gmail.com>
2017-11-30 10:34:05 +0700

    finish admin-catalogs

---
+++ app/admin/catalogs/edit.php
@@ -0,0 +1,48 @@
+<?php session_start(); ?>
+<?php require_once "../../../db/mysql.php"; ?>
+<?php
+  if(!isset($_GET["id"])){
+    $_SESSION["flash"] = "Wrong params";
+    header("location: index.php");
+  }
+  $id = $_GET["id"];
+  $sql = "select * from catalogs where id='$id'";
+  $result = $conn->query($sql);
+  $row = $result->fetch_assoc();
+  $name = $row["name"];
+  $description = $row["description"];
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Chỉnh sửa mục</title>
+    <link rel="stylesheet" type="text/css" href="../../../public/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../public/css/custom.css">
+  </head>
+  <body>
+  <div class="wrapper">
+    <form method="post" action="update.php">
+      <input type="hidden" value="<?php echo $id; ?>" name="id">
+      <div class="row">
+        <i class="flash"><?php if(isset($_SESSION["flash"])) echo $_SESSION["flash"]; ?></i>
+      </div>
+      <div>
+        <h1>Chỉnh sửa danh mục</h1>
+      </div>
+      <div class="row">
+        <label>Tên danh mục:</label>
+        <input class="form-control" type="text" name="name" value="<?php echo $name; ?>">
+      </div>
+      <div class="row">
+        <label>Mô tả:</label>
+        <textarea class="form-control" name="description" value="<?php echo $description; ?>"></textarea>
+      </div>
+      <br>
+      <div class="row">
+        <button class="btn btn-primary">Submit</button>
+      </div>
+    </form>
+  </div>
+  </body>
+</html>
+<?php unset($_SESSION["flash"]); ?>
---
+++ app/admin/catalogs/new.php
@@ -0,0 +1,35 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+  <head>
+    <title>Tạo mới mục</title>
+    <link rel="stylesheet" type="text/css" href="../../../public/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../public/css/custom.css">
+  </head>
+  <body>
+  <div class="wrapper">
+    <form method="post" action="create.php">
+      <div class="row">
+        <i class="flash"><?php if(isset($_SESSION["flash"])) echo $_SESSION["flash"]; ?></i>
+      </div>
+      <div>
+        <h1>Tạo mới chuyên mục</h1>
+      </div>
+      <div class="row">
+        <label>Tên Mục:</label>
+        <input class="form-control" type="text" name="name">
+      </div>
+      <div class="row">
+        <label>Mô tả:</label>
+        <textarea class="form-control" name="description"></textarea>
+      </div>
+      <br>
+      <div class="row">
+        <button class="btn btn-primary">Submit</button>
+      </div>
+    </form>
+  </div>
+  </body>
+</html>
+<?php unset($_SESSION["flash"]); ?>
